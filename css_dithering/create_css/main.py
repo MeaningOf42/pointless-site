e642aee6ff947a06f45833c4604528f89d66e3c8
MeaningOf42 <biltimi@gmail.com>
2018-03-11 13:01:38 +0100

    changed naming to use underscores.

--- css_dithering/create_css/main.py
+++ css_dithering/create_css/main.py
@@ -1,3 +1,5 @@
+from PIL import Image, ImageDraw
+
 css_template = """
 div.kitten {{
   width: 10px;
@@ -34,7 +36,10 @@ def pointsToCSS(points):
     out = out[:-6]+";"
     return out
 
-points = [Point(0,0,"black"), Point(0,1String0,"red"), Point(10,0,"blue")]
+kitten = Image.open("css_dithering/create_css/kitten.jpg")
+kitten.show()
+
+points = [Point(0,0,"black"), Point(0,10,"red"), Point(10,0,"blue")]
 css = css_template.format(pointsToCSS(points))
 
 print(css)
